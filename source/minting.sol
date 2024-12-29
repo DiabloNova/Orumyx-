ebd217fb4820759bb1f994f96cac976ce7a96e05
DiabloNova <121438027+DiabloNova@users.noreply.github.com>
2024-12-29 09:44:04 +0330

    Delete source directory

--- source/minting.sol
+++
@@ -1,7 +0,0 @@
-function mint(address to, uint256 amount) external onlyOwner {
-    orumyxToken.mint(to, amount);
-}
-
-function burn(address from, uint256 amount) external onlyOwner {
-    orumyxToken.burn(from, amount);
-}
