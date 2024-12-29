e9a634a4d37b3aca9cdead9537c5b3f209e3c457
DiabloNova <121438027+DiabloNova@users.noreply.github.com>
2024-12-29 09:43:04 +0330

    Delete contract directory

--- contract/impediment.sol
+++
@@ -1,8 +0,0 @@
-enum ProposalType { FundAllocation, ContractUpgrade, Other }
-ProposalType public proposalType;
-
-if (proposalType == ProposalType.FundAllocation) {
-    // Transfer funds to a designated address
-} else if (proposalType == ProposalType.ContractUpgrade) {
-    // Upgrade the contract (add a function to handle contract upgrades)
-}
